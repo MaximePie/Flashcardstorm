a848084dbdc7f191add6010cb2f5172f0a30ea1d
Maxime Pie <maxime.pie.mail@gmail.com>
2019-11-22 12:56:12 +0100

    fix(front): Fix front empty question issue

--- resources/js/components/QuestionCard.js
+++ resources/js/components/QuestionCard.js
@@ -8,8 +8,8 @@ export default function QuestionCard(props) {
 
   return (
     <div className="QuestionCard card">
-      <p className={"QuestionCard__question " + (!question.wording && "QuestionCard__question--is-empty")}>
-      {question.wording || 'Il n\'y a pas encore de question disponible... Cliquez sur "Add knowledge" pour en ajouter !'}
+      <p className={"QuestionCard__question " + (!question && "QuestionCard__question--is-empty")}>
+      {question || 'Il n\'y a pas encore de question disponible... Cliquez sur "Add knowledge" pour en ajouter !'}
       </p>
       <TextField label="Réponse" onChange={e => setAnswer(e.target.value)}/>
       <Button onClick={() => props.onSubmit(answer)} text="Envoyer"/>
