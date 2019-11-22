c076af1d929da5204cdaefc120ec140444d2dbe6
Maxime Pie <maxime.pie.mail@gmail.com>
2019-11-22 13:36:49 +0100

    fix(front): Fix wrong object display

--- resources/js/components/QuestionCard.js
+++ resources/js/components/QuestionCard.js
@@ -9,7 +9,7 @@ export default function QuestionCard(props) {
   return (
     <div className="QuestionCard card">
       <p className={"QuestionCard__question " + (!question && "QuestionCard__question--is-empty")}>
-      {question || 'Il n\'y a pas encore de question disponible... Cliquez sur "Add knowledge" pour en ajouter !'}
+      {(question && question.wording) || 'Il n\'y a pas encore de question disponible... Cliquez sur "Add knowledge" pour en ajouter !'}
       </p>
       <TextField label="Réponse" onChange={e => setAnswer(e.target.value)}/>
       <Button onClick={() => props.onSubmit(answer)} text="Envoyer"/>
