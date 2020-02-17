cf9a7439abfd77c085ee9f89dca38525b560a0d8
Maxime Pie <maxime.pie.mail@gmail.com>
2020-02-17 18:31:25 +0100

    fix(backoffice): Fix statistics creation

--- app/UserStatistics.php
+++ app/UserStatistics.php
@@ -8,9 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class UserStatistics extends Model
 {
     //
+
+    protected $fillable = [
+        'user_id', 'memorized_questions',
+    ];
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
         $this->created_at = Carbon::today();
+        if (isset($attributes['user_id'])) {
+            $this->memorized_questions = self::query()->where('user_id', $attributes)->latest()->first()->memorized_questions;
+            $this->save();
+        }
     }
 }
