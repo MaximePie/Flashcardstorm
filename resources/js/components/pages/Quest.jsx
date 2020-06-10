f32ea3186edec06d641fc2d3d3012d4bad9d4c40
Chatmalow <maxime.pie.mail@gmail.com>
2020-06-10 16:13:47 +0200

    feat(front): Add dialog box on quest field

--- resources/js/components/pages/Quest.jsx
+++ resources/js/components/pages/Quest.jsx
@@ -16,6 +16,8 @@ export default function Quest() {
 
   const [questions, setQuestions] = React.useState([]);
 
+  const [displayedText, setDisplayedText] = React.useState('');
+
   React.useEffect(() => {
     if (hero.currentLife <= 0) {
       setHeroLife(100);
@@ -36,27 +38,32 @@ export default function Quest() {
   return (
     <div className="Quest">
       {questions[0] && (
-        <div className="Quest__entities">
-          <div className="Quest__entity">
-            <img src={heroSprite} alt="Le héros" className="Quest__hero" />
-            <LinearProgress className="Quest__bar" variant="determinate" value={hero.currentLife} />
-          </div>
-          <QuestionCard
-            question={questions[0] || undefined}
-            onSubmit={(answer) => submitAnswer(answer, questions[0])}
-            key={`QuestionCard-${questions[0].id}`}
-            isQuest
-          />
-          <div className="Quest__entity">
-            <img src={monsterSprite} alt="Le méchant" className="Quest__meany" />
-            <LinearProgress
-              className="Quest__bar"
-              variant="determinate"
-              value={monster.currentLife}
-              color="secondary"
+        <>
+          <div className="Quest__entities">
+            <div className="Quest__entity">
+              <img src={heroSprite} alt="Le héros" className="Quest__hero" />
+              <LinearProgress className="Quest__bar" variant="determinate" value={hero.currentLife} />
+            </div>
+            <QuestionCard
+              question={questions[0] || undefined}
+              onSubmit={(answer) => submitAnswer(answer, questions[0])}
+              key={`QuestionCard-${questions[0].id}`}
+              isQuest
             />
+            <div className="Quest__entity">
+              <img src={monsterSprite} alt="Le méchant" className="Quest__meany" />
+              <LinearProgress
+                className="Quest__bar"
+                variant="determinate"
+                value={monster.currentLife}
+                color="secondary"
+              />
+            </div>
           </div>
-        </div>
+          <div className="Quest__text">
+            {displayedText}
+          </div>
+        </>
       )}
     </div>
   );
@@ -90,8 +97,10 @@ export default function Quest() {
     )
       .then((response) => {
         if (response.data.status === 200) {
+          setDisplayedText('Le monstre perd 10 PV');
           setMonsterLife(monster.currentLife - 10);
         } else if (response.data.status === 500) {
+          setDisplayedText('Le hero perd 10 PV');
           setHeroLife(hero.currentLife - 10);
         }
       });
